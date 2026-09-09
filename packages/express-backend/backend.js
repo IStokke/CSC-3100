23a0c4ed970fa8038bb53b61ed0fd54dba2906e9
Isaiah Stokke <isaiahstokke@gmail.com>
2026-09-09 15:16:29 -0700

    IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -108,5 +108,5 @@ app.get("/users", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}')
+    console.log(`Example app listening at http://localhost:${port}`)
 });
