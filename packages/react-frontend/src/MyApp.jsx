aa800c7003404c666b06fb5d51473d67f4c1091c
Isaiah Stokke <isaiahstokke@gmail.com>
2026-09-09 16:11:14 -0700

    wip IE3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -22,6 +22,18 @@ function MyApp() {
     return promise;
   }
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
@@ -32,7 +44,11 @@ function MyApp() {
 }, []);
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
